9794b3ed867ae8639e77108afbfaa987daad26d4
JanKolo04 <jankolodziej04@gmail.com>
2023-04-26 22:41:50 +0200

    .htaaccess

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <header>
     <div id="name">
-        <h3><a class="link" href="main.php">Week Planer</a></h3>
+        <h3><a class="link" href="index">Week Planer</a></h3>
     </div>
 
     <div id="links">
-        <a class="link" href="all_plans.php">Calendar</a>
-        <a class="link" href="reminder.php">Reminder</a>
-        <a class="link" href="account.php">Account</a>
+        <a class="link" href="all_plans">Calendar</a>
+        <a class="link" href="reminder">Reminder</a>
+        <a class="link" href="account">Account</a>
     </div>
 </header>
--- index.php
+++ index.php
@@ -22,12 +22,12 @@
 
     <div id="stuff">
         <div class="stuff-box" id="calendar-section">
-            <a href="all_plans.php"><div class="photo" id="calendar"></div></a>
+            <a href="all_plans"><div class="photo" id="calendar"></div></a>
             <p>Week calendar is teh best tool to plan your all week to have better performance.</p>
         </div>
 
         <div class="stuff-box" id="reminder-section">
-            <a href="reminder.php"><div class="photo" id="reminder"></div></a>
+            <a href="reminder"><div class="photo" id="reminder"></div></a>
             <p>Reminder can help you to remember all your tasks.</p>
         </div>
     </div>
